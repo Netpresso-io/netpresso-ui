d2be2bb3501ce296ff9bd47c304ec91a684d28c5
Omri Yoffe <omriyoffe366@gmail.com>
2023-07-31 21:42:17 +0300

    ui changes

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -4,12 +4,14 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import Navbar from "../Components/Navigation/Navbar/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
+import SideDrawer from "../Components/Navigation/SideDrawer/SideDrawer";
 
 function App() {
   return (
     <div className="App">
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <Navbar/>
+            <SideDrawer/>
             <Layout/>
         </LocalizationProvider>
     </div>
